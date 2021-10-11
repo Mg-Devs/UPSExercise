4348a8bdaf82ddf7cbb4373a3d871b0a7036e52c
Mario Gonzalez <mario.gonzalez.a2599@gmail.com>
2021-10-11 00:56:27 -0500

    starting rerpository

---
+++ src/main/java/com/ups/exersice/UpsExercise/UpsExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.ups.exersice.UpsExercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UpsExerciseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UpsExerciseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ups/exersice/UpsExercise/UpsExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ups.exersice.UpsExercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UpsExerciseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
